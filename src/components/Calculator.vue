ca2357c4aad1ad385a16c71bfd7726ccafe80e9d
Areeb Jamal <jamal.areeb@gmail.com>
2018-08-15 13:45:22 +0530

    Allow entering digits

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,30 +1,63 @@
 <template>
   <div class="calculator">
-    <div class="display">0.052343</div>
-    <div class="btn">C</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
     <div class="btn operator">÷</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
     <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
     <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
     <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot"  class="btn">.</div>
     <div class="btn operator">=</div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      current: '',
+    }
+  },
+
+  methods: {
+    clear() {
+      this.current = ''
+    },
+
+    sign() {
+      this.current = this.current.charAt(0) == '-' ? 
+        this.current.slice(1) : `-${this.current}`
+    },
+
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`
+    },
+
+    append(number) {
+      if (number === '0' && this.current === '') {
+        return
+      }
+      this.current = `${this.current}${number}`
+    },
+
+    dot() {
+      if (this.current.indexOf('.') === -1) {
+        this.append('.')
+      }
+    }
+  }
 }
 </script>
 
@@ -49,6 +82,7 @@ export default {
 }
 
 .btn {
+  cursor: pointer;
   background-color: #f2f2f2;
   border: 1px solid #999;
 }
