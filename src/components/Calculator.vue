1f0bc5cb6277e66a9aac7d0524dd3063196c869c
Areeb Jamal <jamal.areeb@gmail.com>
2018-08-15 17:51:22 +0530

    Add operator functions

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,22 +4,22 @@
     <div @click="clear" class="btn">C</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div class="btn operator">÷</div>
+    <div @click="divide" class="btn operator">÷</div>
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div class="btn operator">x</div>
+    <div @click="multiply" class="btn operator">x</div>
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
-    <div class="btn operator">-</div>
+    <div @click="subtract" class="btn operator">-</div>
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
     <div @click="append('0')" class="btn zero">0</div>
     <div @click="dot"  class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div @click="equal" class="btn operator">=</div>
   </div>
 </template>
 
@@ -28,12 +28,17 @@ export default {
   data() {
     return {
       current: '',
+      previous: null,
+      operatorCallback: null,
+      operatorClicked: false
     }
   },
 
   methods: {
     clear() {
       this.current = ''
+      this.operatorCallback = null
+      this.previous = null
     },
 
     sign() {
@@ -49,6 +54,10 @@ export default {
       if (number === '0' && this.current === '') {
         return
       }
+      if (this.operatorClicked) {
+        this.current = ''
+        this.operatorClicked = false
+      }
       this.current = `${this.current}${number}`
     },
 
@@ -56,6 +65,39 @@ export default {
       if (this.current.indexOf('.') === -1) {
         this.append('.')
       }
+    },
+
+    operator(callback) {
+      this.equal()
+      this.operatorCallback = callback
+      this.operatorClicked = true
+      this.previous = this.current
+    },
+
+    divide() {
+      this.operator((a, b) => a / b)
+    },
+
+    multiply() {
+      this.operator((a, b) => a * b)
+    },
+
+    subtract() {
+      this.operator((a, b) => a - b)
+    },
+
+    add() {
+      this.operator((a, b) => a + b)
+    },
+
+    equal() {
+      if (!this.operatorCallback || this.operatorClicked || !this.previous)
+        return
+      this.current = `${this.operatorCallback(
+        parseFloat(this.previous), parseFloat(this.current)
+      )}`
+      this.operatorCallback = null
+      this.operatorClicked = true
     }
   }
 }
